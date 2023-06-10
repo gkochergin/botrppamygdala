66297083c0f76e6c48a4d48ea17aff0ea24bd794
Gleb Kochergin <58245191+gkochergin@users.noreply.github.com>
2023-06-10 15:41:12 +0300

    tools.py - added docstring and comments to all functions - added input values check raise errors to functions

--- rpp_bot/bot/tools.py
+++ rpp_bot/bot/tools.py
@@ -18,68 +18,92 @@ def error_logger(func):
     return wrapper
 
 
-lorem_ipsum = "\nMessage 1 at day 3. Very very long content with lorem ipsum.\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\nAliquam ultrices sagittis orci a. Sit amet commodo nulla facilisi. Eu turpis egestas pretium aenean pharetra magna. Nunc sed augue lacus viverra vitae congue eu consequat.\nDolor magna eget est lorem ipsum dolor sit amet consectetur. Sed vulputate mi sit amet. Fringilla ut morbi tincidunt augue interdum velit euismod in. Diam volutpat commodo sed egestas. Amet luctus venenatis lectus magna fringilla urna porttitor rhoncus dolor. Ipsum nunc aliquet bibendum enim facilisis gravida neque convallis a. Fringilla ut morbi tincidunt augue interdum. Eget nunc scelerisque viverra mauris in.\n"
-
-
-def split_text_by_paragraphs(text, max_length) -> list[str] | None:
-    text = text.strip()
-    if text.find('\n') == -1:
-        print(text.find('\n'))
-        return print("No paragraphs to split.")
-    else:
-        paragraphs = [p for p in text.split('\n') if p]
-        print(paragraphs)
-
-        result = []
-        concat_paragraphs = []
-        current_length = 0
-        prev_paragraph_length = 0
-        new_text_block = ''
-
-        for paragraph in paragraphs:
-
-            current_max_length = len(paragraph) + prev_paragraph_length
-
-            if current_max_length < max_length:
-                print(f"\nMax length: {max_length} > Paragraph length: {len(paragraph)}")
-                concat_paragraphs = concat_paragraphs.append(paragraph)
-                new_text_block = '\n'.join(concat_paragraphs)
-                prev_paragraph_length = len(new_text_block)
-            else:
-                if new_text_block:
-                    result.append(new_text_block)
-                else:
-                    result.append(paragraph)
-        print(result)
-        # if current_part:
-        #     result.append(current_part)
-        return result
-
-
 def split_text_by_sentences(text: str, max_length: int):
-    text = text.strip()
-    sentences = sent_tokenize(text)
+    """
+    Splits a given text into sentences and groups them into parts with a maximum length.
+
+    :param text: The text to split.
+    :type text: str
+    :param max_length: The maximum length of each part.
+    :type max_length: int
+    :return: A list of strings where each string is a part of the original text with a maximum length.
+    :rtype: List[str]
+    """
+    # Check input values
+    if not text:
+        raise ValueError("Input text is empty")
+    if max_length <= 0:
+        raise ValueError("Variable max_length can't be 0 or negative number")
+
+    # Initialize the result list, current part list, and current length counter
     result = []
     current_part = []
     current_length = 0
+
+    # Remove leading and trailing whitespace from the text
+    text = text.strip()
+
+    # Split the text into sentences with nltk library
+    sentences = sent_tokenize(text)
+
+    # Iterate over the sentences
     for sentence in sentences:
+        # If adding the current sentence to the current part would exceed the maximum length,
+        # add the current part to the "result" list and reset the "current_part" and "current_length" counters
         if current_length + len(sentence) + 1 > max_length:
             result.append(' '.join(current_part))
             current_part = []
             current_length = 0
+
+        # Add the current sentence to the current part and update the length counter
         current_part.append(sentence)
         current_length += len(sentence) + 1
+
+    # If there is a non-empty current part, add it to the result list
     if current_part:
         result.append(' '.join(current_part))
+
     return result
 
 
 def convert_response_dict_string(response: list) -> str:
-    result = ''
-    if response:
-        content_list = []
-        for dictionary in response:
-            if dictionary['content_type'] == 'TXT':
-                content_list.append(dictionary['content'])
-        result = '\n\n'.join(content_list)
+    """
+    Converts a list of dictionaries into a string by extracting the 'content' values of dictionaries with 'content_type' equal to 'TXT'.
+
+    :param response: A list of dictionaries containing 'content_type' and 'content' keys.
+    :type response: list
+    :return: A string containing the extracted 'content' values separated by two newlines.
+    :rtype: str
+    :raises TypeError: If the input response is not a list or if any element of the response list is not a dictionary.
+    :raises KeyError: If any dictionary in the response list does not contain the required keys 'content_type' and 'content'.
+    :raises ValueError: If the input response is empty.
+    """
+    # Check if the input response is empty and is a list
+    if not response:
+        raise ValueError("Input response is empty")
+    elif not isinstance(response, list):
+        raise TypeError(f"Expected a list, but got {type(response)}")
+
+    # Initialize a list to store the extracted content values
+    content_list = []
+
+    # Iterate over the dictionaries in the response list
+    for dictionary in response:
+        # Check if the current element is a dictionary and contains the required keys
+        if not isinstance(dictionary, dict):
+            raise TypeError(f"Expected a dictionary, but got {type(dictionary)}")
+        elif 'content_type' not in dictionary or 'content' not in dictionary:
+            raise KeyError(f"Missing required keys 'content_type' and/or 'content' in dictionary:\n{dictionary}")
+
+        # If the 'content_type' value is 'TXT', append the 'content' value to the content list
+        if dictionary['content_type'] == 'TXT':
+            content_list.append(dictionary['content'])
+
+    # Join the content list with two newlines as separator and assign it to the result string
+    result = '\n\n'.join(content_list)
+
+    # Return the result string
     return result
+
+
+lorem_ipsum = "\nMessage 1 at day 3. Very very long content with lorem ipsum.\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\nAliquam ultrices sagittis orci a. Sit amet commodo nulla facilisi. Eu turpis egestas pretium aenean pharetra magna. Nunc sed augue lacus viverra vitae congue eu consequat.\nDolor magna eget est lorem ipsum dolor sit amet consectetur. Sed vulputate mi sit amet. Fringilla ut morbi tincidunt augue interdum velit euismod in. Diam volutpat commodo sed egestas. Amet luctus venenatis lectus magna fringilla urna porttitor rhoncus dolor. Ipsum nunc aliquet bibendum enim facilisis gravida neque convallis a. Fringilla ut morbi tincidunt augue interdum. Eget nunc scelerisque viverra mauris in.\n"
