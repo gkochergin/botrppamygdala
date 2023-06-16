3dbf878697736895251fe22c6c9376f178ed5d4f
Gleb Kochergin <58245191+gkochergin@users.noreply.github.com>
2023-06-16 23:20:49 +0300

    eating_habbits.py - updated all docstrings and comments

--- rpp_bot/bot/quiz/eating_habbits.py
+++ rpp_bot/bot/quiz/eating_habbits.py
@@ -11,6 +11,13 @@ router = Router()
 
 
 class QuizSession:
+    """
+    A class representing a quiz session for a user.
+
+    :param user_id: The Telegram ID of the user taking the quiz.
+    :param score: The current quiz score of the user.
+    :param question_number: The current question number.
+    """
     questions = api.get_quiz_question_list()
     question_count = len(questions)
     quiz_name = "<b>Тест на наличие проблем с пищевым поведением</b>"
@@ -29,9 +36,15 @@ class QuizSession:
                               f"результат. Ты ответила \"Да\":"
 
     def add_one_to_score(self) -> None:
+        """
+        Increment the user's score by one.
+        """
         self.score += 1
 
     def save_quiz_result(self) -> None:
+        """
+        Save the user's quiz result using the API.
+        """
         # через api создаем в бд объект с результатом прохождения теста
         try:
             api.save_quiz_result(user_id=self.user_id, result=self.score)
@@ -40,12 +53,23 @@ class QuizSession:
             print(e)
 
     def get_question(self, increase: int) -> str:
+        """
+        Get the current question and increment the question number.
+
+        :param increase: The amount to increment the question number by.
+        :return: The current question text.
+        """
         if self.question_number <= self.question_count - 1:
             question = self.questions[self.question_number][self.__field_name]
             self.question_number += increase
             return question
 
     def keyboard(self):
+        """
+        Create an inline keyboard markup for the quiz.
+
+        :return: An InlineKeyboardMarkup object representing the constructed inline keyboard markup.
+        """
         btns = [{'name': 'Да', 'data': 'yes'}, {'name': 'Нет', 'data': 'no'}]
         markup = kb.make_inline_kb(buttons_data=btns)
         return markup
@@ -56,20 +80,38 @@ USER_SESSION: QuizSession
 
 @router.message(Command(commands='myhabbits'))
 async def start_quiz(message: types.Message):
+    """
+    Start a quiz session for the user.
+
+    :param message: The message object representing the user's message.
+    """
     global USER_SESSION
 
-    # инициализируем данные сессии
+    # Initialize the user's quiz session
     USER_SESSION = QuizSession(user_id=str(message.from_user.id), question_number=0, score=0)
 
-    # Сообщаем пользователю, что тест начат
+    # Notify the user that the quiz has started
     await message.answer(USER_SESSION.quiz_name)
+
+    # Show the first question
     await quiz_show_question(message=message, increase=1)
 
 
 async def quiz_show_question(message: types.Message, increase: int):
+    """
+    Show the current quiz question to the user.
+
+    :param message: The message object representing the user's message.
+    :param increase: The amount to increment the question number by.
+    """
     global USER_SESSION
+    # Get the current question text
     question_text = USER_SESSION.get_question(increase=increase)
+
+    # Format the message text
     message_text = f"<b>Вопрос {USER_SESSION.question_number} из {USER_SESSION.question_count}</b>\n\n{question_text}"
+
+    # Edit or send the message
     if USER_SESSION.question_number > 1:
         await message.edit_text(text=message_text, reply_markup=USER_SESSION.keyboard())
     else:
@@ -78,25 +120,40 @@ async def quiz_show_question(message: types.Message, increase: int):
 
 @router.callback_query(F.data.in_(['yes', 'no']))
 async def process_callback(callback_query: types.CallbackQuery):
+    """
+    Process a callback query from an inline keyboard button.
+
+    :param callback_query: The callback query object representing the user's interaction with the inline keyboard.
+    """
     global USER_SESSION
 
+    # Check if the quiz is not finished
     if not USER_SESSION.finished:
+        # Update the user's score based on their answer
         if callback_query.data == "yes":
             USER_SESSION.add_one_to_score()
         elif callback_query.data == "no":
             pass
 
+    # Check if there is a next question
     if USER_SESSION.get_question(increase=0) and not USER_SESSION.finished:
+        # Show the next question
         await quiz_show_question(message=callback_query.message, increase=1)
         await callback_query.answer(show_alert=False)
     else:
+        # Determine the result text based on the user's score
         if USER_SESSION.score <= 2:
             result_text = USER_SESSION.result_ok
         else:
             result_text = USER_SESSION.result_bad
+
+        # Send the result message if the quiz is not finished
         if not USER_SESSION.finished:
             await callback_query.message.answer(
                 f"{USER_SESSION.result_analyze} <b>{USER_SESSION.score}</b>\n\n{result_text}")
+
+        # Save the user's quiz result and mark the quiz as finished
         USER_SESSION.save_quiz_result()
         USER_SESSION.finished = True
+
     await callback_query.answer(show_alert=False)
