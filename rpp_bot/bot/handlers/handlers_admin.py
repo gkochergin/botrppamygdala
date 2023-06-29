a72527824d02d6410643e3db78a9bd46e7b57d5f
Gleb Kochergin <58245191+gkochergin@users.noreply.github.com>
2023-06-29 15:33:01 +0300

    обновлен способ вызова библиотек для handlers_admin.py по относительному пути добавлена строка логирования

--- rpp_bot/bot/handlers/handlers_admin.py
+++ rpp_bot/bot/handlers/handlers_admin.py
@@ -13,6 +13,8 @@ data_storage = DataStorage()
 @router.message(Command(commands=['admindays']))
 @admin_access
 async def command_day_handler(message: types.Message, ds=data_storage) -> None:
+    log_text = f'User id {message.from_user.id} called {command_day_handler.__name__} function under the admin rights. '
+    logging.info(log_text)
     await message.answer('Выберите нужный день',
                          reply_markup=make_row_keyboard(
                              items=ds.btn_days_list,
